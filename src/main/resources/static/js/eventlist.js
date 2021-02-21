7fbe0010c3a8b7ca6064a6feb7147c7bee980cc0
cyrusmcc <cyrusmcc@hotmail.com>
2021-02-21 01:03:38 -0700

    Remove task list & current task bar
    Add calendar header
    
    Add button bar for goals and events
    
    Restructure structure so that journal is now under calendar

--- src/main/resources/static/js/eventlist.js
+++ src/main/resources/static/js/eventlist.js
@@ -15,7 +15,8 @@ for(i = 0; i < userEvents.length; i++) {
         eventDiv.style.background = '#9a7092';
         eventDiv.style.color = '#0d0d0e';
         eventDiv.style.fontWeight = "bold";
-        eventDiv.style.margin = "0 0.5em 0.3em";
+        eventDiv.style.borderRadius = "3px"
+        eventDiv.style.margin = "0 0.5em 1em";
         eventDiv.style.padding = "5px";
 
         document.getElementById("eventListContents").appendChild(eventDiv);
