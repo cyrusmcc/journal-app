9500fb6ddb309ca82233a12603d368ed5f4bbc47
cyrusmcc <cyrusmcc@hotmail.com>
2021-02-20 18:12:46 -0700

    Refactor journal structure

--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -10,9 +10,13 @@ var childDivs = document.getElementById('calendarGrid')
 var userEvents = JSON.parse(document.getElementById("userEvents").innerText);
 
 var d = new Date();
+
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-var calendarTitle = days[d.getDay()] + ", " + month + " " + dayOfMonth + ", " + d.getFullYear();
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September",
+              "October", "November", "December"];
+
+var calendarTitle = days[d.getDay()] + ", " + months[d.getMonth()] + " " + dayOfMonth + ", " + d.getFullYear();
 document.getElementById('dateDisplay').innerText = calendarTitle;
 
 var activeUserTasks = 0;
@@ -48,26 +52,24 @@ for(i=1; i < childDivs.length; i++ ) {
         // display task bubble on current date
         if(activeUserTasks > 0) {
 
-            console.log(activeUserTasks);
-        var numberOfTaskCircle = document.createElement("div");
+            var numberOfTaskCircle = document.createElement("div");
 
+            numberOfTaskCircle.style.justifySelf = "flex-start";
+            numberOfTaskCircle.style.gridArea = "grid-area: 1 / 1 / 3 / 3";
+            numberOfTaskCircle.style.textAlign = "center";
+            numberOfTaskCircle.style.marginBottom = "1em";
+            numberOfTaskCircle.style.background = '#fbf7f4';
+            numberOfTaskCircle.style.color = '#1b1b1b';
+            numberOfTaskCircle.style.borderRadius = "50%";
+            numberOfTaskCircle.style.fontSize = ".8em";
+            numberOfTaskCircle.style.fontWeight = "bold";
+            numberOfTaskCircle.style.width = "1.2em";
+            numberOfTaskCircle.style.height = "1.2em";
+            numberOfTaskCircle.style.margin = "0.3em";
+            numberOfTaskCircle.style.paddingBottom = "0.15em";
+            numberOfTaskCircle.innerText = activeUserTasks;
 
-        numberOfTaskCircle.style.justifySelf = "flex-start";
-        numberOfTaskCircle.style.gridArea = "grid-area: 1 / 1 / 3 / 3";
-        numberOfTaskCircle.style.textAlign = "center";
-        numberOfTaskCircle.style.marginBottom = "1em";
-        numberOfTaskCircle.style.background = '#fbf7f4';
-        numberOfTaskCircle.style.color = '#a698da';
-        numberOfTaskCircle.style.borderRadius = "50%";
-        numberOfTaskCircle.style.fontSize = ".8em";
-        numberOfTaskCircle.style.fontWeight = "bold";
-        numberOfTaskCircle.style.width = "1.2em";
-        numberOfTaskCircle.style.height = "1.2em";
-        numberOfTaskCircle.style.margin = "0.3em";
-        numberOfTaskCircle.style.paddingBottom = "0.15em";
-        numberOfTaskCircle.innerText = activeUserTasks;
-
-        document.getElementById(dayGridBox).appendChild(numberOfTaskCircle);
+            document.getElementById(dayGridBox).appendChild(numberOfTaskCircle);
         }
 
         document.getElementById(dayGridBox).style.background = '#4a94b7';
